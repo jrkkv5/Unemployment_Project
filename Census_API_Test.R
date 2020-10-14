7db111d2f62376302524544ab9a89db50b2c2bdb
Jeff Krueger <jrkkv5@gmail.com>
2020-10-13 21:53:34 -0500

    Commit for changes on 10.13.2020

--- Census_API_Test.R
+++ Census_API_Test.R
@@ -26,6 +26,7 @@ View(qwi)
 
 require(httr)
 require(jsonlite)
-getdata = httr::GET("https://api.census.gov/data/timeseries/qwi/sa?get=Emp,EmpS,HirA,EarnS,Sep&time=2012-Q1&for=state:02,24,01&sex=1&sex=2&agegrp=A02&agegrp=A07&ownercode=A00&seasonadj=U&industry=11&key=1c255c9c3fb15e6f197dd34a721d696d0bd28539")
-View(getdata)
-httr::GET("https://api.census.gov/data/2017/acs/acs1?get=NAME,group(B01001)&for=us:1")
+httr::GET("https://api.census.gov/data/timeseries/qwi/sa?get=Emp,EmpS,HirA,EarnS,Sep&time=2012-Q1&for=state:02,24,01&sex=1&sex=2&agegrp=A02&agegrp=A07&ownercode=A00&seasonadj=U&industry=11&key=1c255c9c3fb15e6f197dd34a721d696d0bd28539")
+
+raw = GET("https://api.census.gov/data/timeseries/qwi/sa?get=Emp,EmpS,HirA,EarnS,Sep&time=2012-Q1&for=state:02,24,01&sex=1&sex=2&agegrp=A02&agegrp=A07&ownercode=A00&seasonadj=U&industry=11&key=1c255c9c3fb15e6f197dd34a721d696d0bd28539")
+View(raw)
